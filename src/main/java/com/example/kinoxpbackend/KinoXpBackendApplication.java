f1749f83963c24b6ad548918d24afcd576b2b33b
masp1234 <mart2c04@stud.kea.dk>
2022-10-04 11:22:43 +0200

    KX-13 H2 database sat op>

--- src/main/java/com/example/kinoxpbackend/KinoXpBackendApplication.java
+++ src/main/java/com/example/kinoxpbackend/KinoXpBackendApplication.java
@@ -1,13 +1,29 @@
 package com.example.kinoxpbackend;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class KinoXpBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(KinoXpBackendApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner importData( //repositories
+
+            ) {
+
+        return (args) -> {
+
+            // whatever du har lyst til
+
+        };
     }
 
 }
