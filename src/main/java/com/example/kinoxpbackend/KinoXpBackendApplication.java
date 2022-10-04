813c3d8cdc83e7a801fa4b677c59c5256634fecb
masp1234 <mart2c04@stud.kea.dk>
2022-10-04 14:03:40 +0200

    KX-16 add FilmController and FilmService

--- src/main/java/com/example/kinoxpbackend/KinoXpBackendApplication.java
+++ src/main/java/com/example/kinoxpbackend/KinoXpBackendApplication.java
@@ -36,6 +36,13 @@ public class KinoXpBackendApplication {
             film1.setDescription("En film om en båd");
             film1.setLengthInMinutes(140);
 
+            Film film2 = new Film();
+            film2.setTitle("Avatar");
+            film2.setGenre("Eventyr");
+            film2.setMinimumAge(12);
+            film2.setDescription("En film om en anden planet");
+            film2.setLengthInMinutes(160);
+
 
 
 
@@ -47,7 +54,10 @@ public class KinoXpBackendApplication {
             actorRepository.save(actor);
 
             film1.setActors(Arrays.asList(actor));
+            film2.setActors(Arrays.asList(actor));
             filmRepository.save(film1);
+
+            filmRepository.save(film2);
 
 
 
--- src/main/java/com/example/kinoxpbackend/controllers/FilmController.java
+++ src/main/java/com/example/kinoxpbackend/controllers/FilmController.java
@@ -1,4 +1,35 @@
 package com.example.kinoxpbackend.controllers;
 
+
+import com.example.kinoxpbackend.models.Film;
+import com.example.kinoxpbackend.services.FilmService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/film")
+
 public class FilmController {
+
+    private FilmService filmService;
+
+    public FilmController(FilmService filmService) {
+        this.filmService = filmService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Film> getFilmById(@PathVariable("id") Long id) {
+        Film film = filmService.getFilmById(id);
+        return new ResponseEntity<>(film, HttpStatus.OK);
+    }
+    @GetMapping("/all")
+    public ResponseEntity<List<Film>> getAllFilms() {
+        List<Film> films = filmService.findAllFilms();
+
+        return new ResponseEntity<>(films, HttpStatus.OK);
+    }
 }
