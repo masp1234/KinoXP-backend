378da53eff91a84d644dc7183a225d69fa69bdc3
DanielKoltze <dani64h6@stud.kea.dk>
2022-10-04 15:02:53 +0200

    crossorigin

--- src/main/java/com/example/kinoxpbackend/controllers/FilmController.java
+++ src/main/java/com/example/kinoxpbackend/controllers/FilmController.java
@@ -1,4 +1,5 @@
 package com.example.kinoxpbackend.controllers;
 
+
 public class FilmController {
 }
--- src/main/java/com/example/kinoxpbackend/controllers/UserController.java
+++ src/main/java/com/example/kinoxpbackend/controllers/UserController.java
@@ -5,6 +5,7 @@ import com.example.kinoxpbackend.services.UserService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin
 public class UserController {
 UserService userService;
 
