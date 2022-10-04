47d723ca01be84f3dab83328675f21916ab8e461
DNY <dannylu1999@gmail.com>
2022-10-04 11:57:33 +0200

    Initial commit

--- src/main/java/com/example/kinoxpbackend/controllers/UserController.java
+++ src/main/java/com/example/kinoxpbackend/controllers/UserController.java
@@ -1,4 +1,23 @@
 package com.example.kinoxpbackend.controllers;
 
+import com.example.kinoxpbackend.models.User;
+import com.example.kinoxpbackend.services.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("add/user")
+    public void addUser(@RequestParam("name") String name,@RequestParam("username") String username,@RequestParam("password") String password,@RequestParam("type") String type){
+        User user = new User(name, username, password, type);
+        System.out.println(user);
+        userService.saveUser(user);
+    }
 }
--- src/main/java/com/example/kinoxpbackend/models/User.java
+++ src/main/java/com/example/kinoxpbackend/models/User.java
@@ -1,4 +1,41 @@
 package com.example.kinoxpbackend.models;
 
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(name = "user_name", nullable = false)
+    private String name;
+
+    @Column(name = "user_username", nullable = false)
+    private String username;
+
+    @Column(name = "user_password", nullable = false)
+    private String password;
+
+    @Column(name = "user_type", nullable = false)
+    private String type; //Måske ændre til int
+
+    public User(String name, String username, String password, String type) {
+        this.name = name;
+        this.username = username;
+        this.password = password;
+        this.type = type;
+    }
 }
