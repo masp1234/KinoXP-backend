609b7468706462cada30e4399ef330eea6921951
RenasAli <rena0506@stud.kea.dk>
2022-10-06 13:39:00 +0200

    Room Entity.

--- src/main/java/com/example/kinoxpbackend/models/Room.java
+++ src/main/java/com/example/kinoxpbackend/models/Room.java
@@ -1,10 +1,15 @@
 package com.example.kinoxpbackend.models;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 
-@SuppressWarnings("JpaAttributeTypeInspection")
+@NoArgsConstructor
+@Setter
+@Getter
 @Entity
 @Table(name= "ROOM")
 public class Room {
--- src/main/java/com/example/kinoxpbackend/models/Seat.java
+++ src/main/java/com/example/kinoxpbackend/models/Seat.java
@@ -7,7 +7,7 @@ import lombok.*;
 import javax.persistence.*;
 import java.util.List;
 
-@SuppressWarnings("JpaAttributeTypeInspection")
+
 @Entity
 @Getter
 @Setter
