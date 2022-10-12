a57d0424f6405c462654b7d419945800d4e88794
DanielKoltze <dani64h6@stud.kea.dk>
2022-10-12 12:14:06 +0200

    fjernet test

--- src/test/java/com/example/kinoxpbackend/KinoXpBackendApplicationTests.java
+++ src/test/java/com/example/kinoxpbackend/KinoXpBackendApplicationTests.java
@@ -3,10 +3,10 @@ package com.example.kinoxpbackend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class KinoXpBackendApplicationTests {
 
-    @Test
+    //@Test
     void contextLoads() {
     }
 
